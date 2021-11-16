1a449fdcf7ff993ca6aa8def97021c148e7d4f07
Simon Leonard <sleonard@jhu.edu>
2021-11-16 01:05:38 -0500

    added launch file + fixes

--- src/spine_control.cpp
+++ src/spine_control.cpp
@@ -1,5 +1,6 @@
 #include <memory>
 #include <iostream>
+#include <iomanip>
 
 #include "rclcpp/rclcpp.hpp"
 
@@ -41,6 +42,8 @@ private:
   bool valid_entry;
   bool valid_target;
 
+  moveit::planning_interface::MoveGroupInterface* move_group;
+  moveit::core::RobotStatePtr current_state;  
 public:
 
   spine_control( ) :
@@ -49,30 +52,44 @@ public:
     valid_target(false){
     
     sub_igtl_point = this->create_subscription<ros2_igtl_bridge::msg::Point>
-      ("/IGTL_POINT_IN", 10, std::bind(&spine_control::igtl_point_cb, this, std::placeholders::_1));
+      ("/IGTL_POINT_IN", 10,
+       std::bind(&spine_control::igtl_point_cb, this,std::placeholders::_1));
 
-    this->declare_parameter<std::string>("robot_description", "world");
-    rclcpp::SyncParametersClient::SharedPtr param_client = std::make_shared<rclcpp::SyncParametersClient>(this, "robot_state_publisher");
-    while(!param_client->wait_for_service(1s)){}
-    std::string robot_description = param_client->get_parameter<std::string>("robot_description");
-    std::vector<rclcpp::Parameter> params;
-    params.push_back( rclcpp::Parameter("robot_description", robot_description ) );
-    this->set_parameters(params);
+    //this->declare_parameter<std::string>("robot_description", "default");
+    //this->declare_parameter<std::string>("robot_description_semantic", "default");
+    //rclcpp::SyncParametersClient::SharedPtr param_client = std::make_shared<rclcpp::SyncParametersClient>(this, "move_group");
+    //while(!param_client->wait_for_service(1s)){}
+    //std::string robot_description = param_client->get_parameter<std::string>("robot_description");
+    //std::string robot_semantic = param_client->get_parameter<std::string>("robot_description_semantic");
+    //std::vector<rclcpp::Parameter> params;
+    //params.push_back( rclcpp::Parameter("robot_description", robot_description ) );
+    //params.push_back( rclcpp::Parameter("robot_description_semantic", robot_semantic ) );
+    //this->set_parameters(params);
   }
 
-  void igtl_point_cb( const ros2_igtl_bridge::msg::Point::SharedPtr point ){
+  void create_mg(){
+    // cannot use shared_from_this() in constructor :(
+    move_group = new moveit::planning_interface::MoveGroupInterface
+      (shared_from_this(), "ur_manipulator");
 
+    current_state=move_group->getCurrentState();
+    current_state->printStatePositions(std::cout);
+  }
+  
+  void igtl_point_cb( const ros2_igtl_bridge::msg::Point::SharedPtr point ){
     if(point->name.compare("Entry") == 0){
       valid_entry = true;
-      entry.x = point->pointdata.x / 1000.0; // Convert mm to m
-      entry.y = point->pointdata.y / 1000.0; // Convert mm to m
-      entry.z = point->pointdata.z / 1000.0; // Convert mm to m      
+      // -603.73 401.57 150.40
+      entry.x = -603.73/1000; //point->pointdata.x / 1000.0; // Convert mm to m
+      entry.y =  401.57/1000; //point->pointdata.y / 1000.0; // Convert mm to m
+      entry.z =  150.40/1000; //point->pointdata.z / 1000.0; // Convert mm to m      
       RCLCPP_INFO(LOGGER, "[spine_control] Entry point has been received: \n");
     } else if(point->name.compare("Target") == 0){
+      // -608.74 393.21 135.82
       valid_target = true;
-      target.x = point->pointdata.x / 1000.0; // Convert mm to m
-      target.y = point->pointdata.y / 1000.0; // Convert mm to m
-      target.z = point->pointdata.z / 1000.0; // Convert mm to m      
+      target.x = -608.74/1000; //point->pointdata.x / 1000.0; // Convert mm to m
+      target.y =  393.21/1000; //point->pointdata.y / 1000.0; // Convert mm to m
+      target.z =  135.82/1000; //point->pointdata.z / 1000.0; // Convert mm to m      
       RCLCPP_INFO(LOGGER, "[spine_control] Target point has been received: \n");
     }
     if( valid_entry && valid_target ){
@@ -81,15 +98,7 @@ public:
   }
 
   void execute(){
-
-    rclcpp::NodeOptions node_options;
-    node_options.automatically_declare_parameters_from_overrides(true);
-
     std::string PLANNING_GROUP = "ur_manipulator";
-    moveit::planning_interface::MoveGroupInterface
-      move_group(shared_from_this(), PLANNING_GROUP);
-    moveit::core::RobotStatePtr current_state = move_group.getCurrentState();
-    std::cout << current_state << std::endl;
 
     tf2::Vector3 p1( entry.x, entry.y, entry.z ); 
     tf2::Vector3 p2( target.x, target.y, target.z ); 
@@ -98,11 +107,21 @@ public:
 
     tf2::Vector3 o( 0.0, 1.0, 0.0 );
     tf2::Vector3 n = o.cross(a);
-      
+    /*
     tf2::Matrix3x3 R( n.getX(), o.getX(), a.getX(),
 		      n.getY(), o.getY(), a.getY(),
 		      n.getZ(), o.getZ(), a.getZ() );
-
+    */
+    tf2::Matrix3x3 R( -0.9466,   -0.1493,   -0.2858,
+		      0.0039,    0.8810,   -0.4730,
+		      0.3224,   -0.4489,   -0.8334 );
+    /*
+    std::cout << std::setw(13) << n.getX() << std::setw(13) << o.getX() << std::setw(13) << a.getX() << std::endl;
+    std::cout << std::setw(13) << n.getY() << std::setw(13) << o.getY() << std::setw(13) << a.getY() << std::endl;
+    std::cout << std::setw(13) << n.getZ() << std::setw(13) << o.getZ() << std::setw(13) << a.getZ() << std::endl<< std::endl;;
+    std::cout << std::setw(13) << p1.getX() << std::setw(13) << p1.getY() << std::setw(13) << p1.getZ() << std::endl;
+    */
+    
     geometry_msgs::msg::PoseStamped pose;
     tf2::Transform tfpose(R, p1);
     pose.pose.position.x = tfpose.getOrigin().getX();
@@ -112,8 +131,6 @@ public:
     pose.pose.orientation.y = tfpose.getRotation().getY();
     pose.pose.orientation.z = tfpose.getRotation().getZ();
     pose.pose.orientation.w = tfpose.getRotation().getW();
-    //pose.pose = tf2::toMsg<tf2::Transform,geometry_msgs::msg::PoseStamped>(tfpose);
-    //tf2::convert( tfpose, pose);
     
     sensor_msgs::msg::JointState seed;
     seed.name.push_back( "shoulder_pan_joint" );
@@ -122,14 +139,14 @@ public:
     seed.name.push_back( "wrist_1_joint" );
     seed.name.push_back( "wrist_2_joint" );
     seed.name.push_back( "wrist_3_joint" );
-    seed.position.push_back( -0.4735596817074883 );
-    seed.position.push_back( -1.7595926907275998 );
-    seed.position.push_back( 2.6589746902651235 );
-    seed.position.push_back( -0.9003782216382002 );
-    seed.position.push_back( -0.506688355320275 );
-    seed.position.push_back( -0.7767779676822881 );
+    seed.position.push_back( 174.65*M_PI/180.0 );
+    seed.position.push_back(-101.60*M_PI/180.0 );
+    seed.position.push_back(-134.21*M_PI/180.0 );
+    seed.position.push_back(  -7.01*M_PI/180.0 );
+    seed.position.push_back(  30.34*M_PI/180.0 );
+    seed.position.push_back( -54.57*M_PI/180.0 );
 
-    auto request = std::make_shared<moveit_msgs::srv::GetPositionIK::Request>();
+    auto request=std::make_shared<moveit_msgs::srv::GetPositionIK::Request>();
     request->ik_request.group_name = PLANNING_GROUP;
     request->ik_request.robot_state.joint_state = seed;
     request->ik_request.robot_state.is_diff = false;
@@ -137,91 +154,97 @@ public:
     request->ik_request.pose_stamped = pose;
     //request->ik_request.timeout = ros::Duration(5);
 
-    //rclcpp::Client<moveit_msgs::srv::GetPositionIK>::SharedPtr client =
-    //  this->create_client<moveit_msgs::srv::GetPositionIK>("compute_ik");
-    //auto result = client->async_send_request(request);
-
-    //if (rclcpp::spin_until_future_complete(shared_from_this(), result) ==
-    //rclcpp::FutureReturnCode::SUCCESS){
-      moveit_msgs::msg::RobotTrajectory trajectory;
-      move_group.setStartState(*current_state);
-      move_group.setJointValueTarget(seed);
-      //move_group.setJointValueTarget(result.get()->solution.joint_state);
-      moveit::planning_interface::MoveGroupInterface::Plan plan;
-      moveit::planning_interface::MoveItErrorCode code = move_group.plan( plan );
-      if( (bool)code ){
-	move_group.execute(plan);
-      }
-      //}
-      //else {}
-  }
-  
-  //void result_cb( const moveit_msgs::msg::ExecuteTrajectoryActionResult& result ){
     /*
-    ros::AsyncSpinner spinner(1);
-    spinner.start();
-    std::string PLANNING_GROUP = "ur5";
-    moveit::planning_interface::MoveGroupInterface move_group(PLANNING_GROUP);
-    
-    geometry_msgs::PoseStamped current_pose = move_group.getCurrentPose( "needle" );
-
-    geometry_msgs::Pose desired_pose = current_pose.pose;
-    desired_pose.position = target;
-
-    std::vector<geometry_msgs::Pose> waypoints;
-    waypoints.push_back(current_pose.pose);
-    waypoints.push_back(desired_pose);
-
-
-    ros::Duration(4).sleep();
-    
-    const double jump_threshold = 0.0;
-    const double eef_step = 0.005;
-    moveit_msgs::RobotTrajectory trajectory;
-    double fraction = move_group.computeCartesianPath(waypoints, eef_step, jump_threshold, trajectory);
-
-    for( int i=0; i<trajectory.joint_trajectory.points.size(); i++ ){
-      for( int j=0; j<trajectory.joint_trajectory.points[i].positions.size(); j++){
-	//std::cout << std::setw(13) << trajectory.joint_trajectory.points[i].positions[j];
-      }
-      trajectory.joint_trajectory.points[i].time_from_start *= 4.0;  // slow motion down
-    }
-
-    actionlib::SimpleActionClient<moveit_msgs::ExecuteTrajectoryAction> client("/execute_trajectory", true);
-    std::cout << "Wait for server" << std::endl;
-    client.waitForServer();    
-
-    moveit_msgs::ExecuteTrajectoryGoal goal;
-    goal.trajectory.joint_trajectory = trajectory.joint_trajectory;
-    client.sendGoal(goal);
-    bool finished_before_timeout = client.waitForResult(ros::Duration(60.0));
-    if(finished_before_timeout){
-      actionlib::SimpleClientGoalState state = client.getState();
-      ROS_INFO("Action finished: %s",state.toString().c_str());
-    }
+    rclcpp::Client<moveit_msgs::srv::GetPositionIK>::SharedPtr client =
+      this->create_client<moveit_msgs::srv::GetPositionIK>("compute_ik");
+    auto result = client->async_send_request(request);
+    std::shared_ptr<rclcpp::Node> tmp = rclcpp::Node::make_shared("tmp");
+    if(rclcpp::spin_until_future_complete(tmp, result) ==
+     rclcpp::FutureReturnCode::SUCCESS){
     */
-  //}
+    move_group->setStartState(*current_state);
+    //move_group->setJointValueTarget(result.get()->solution.joint_state);
+    move_group->setJointValueTarget(seed);
+    moveit::planning_interface::MoveGroupInterface::Plan plan;
+    moveit::planning_interface::MoveItErrorCode code=move_group->plan(plan);
+    if( (bool)code ){
+      std::cout << "Moving to entry" << std::endl;
+      move_group->execute(plan);
+      std::cout << "Entry point reached" << std::endl;
+      geometry_msgs::msg::Pose current_pose = pose.pose;
+      geometry_msgs::msg::Pose desired_pose = pose.pose;
+      desired_pose.position = target;
 
+      std::vector<geometry_msgs::msg::Pose> waypoints;
+      waypoints.push_back(current_pose);
+      waypoints.push_back(desired_pose);
+      
+      const double jump_threshold = 1;
+      const double eef_step = 0.001;
+      moveit_msgs::msg::RobotTrajectory trajectory;
+      move_group->setPoseReferenceFrame("base");
+      move_group->setEndEffectorLink("tip");
+      move_group->setPoseTarget(desired_pose);
+      
+      /*
+      double fraction = move_group->computeCartesianPath(waypoints,
+							 eef_step,
+							 jump_threshold, trajectory,
+							 false,
+							 &code);
+      // disable collision for this one because the model is a little bit off.
+      
+      std::cout << "fraction " << fraction << " " << code.val << std::endl;
+      for( int i=0; i<trajectory.joint_trajectory.points.size(); i++ ){
+	for( int j=0; j<trajectory.joint_trajectory.points[i].positions.size(); j++){
+	  std::cout << std::setw(13) << trajectory.joint_trajectory.points[i].positions[j];
+	}
+	trajectory.joint_trajectory.points[i].time_from_start.sec *= 4;
+      }
+      */
+
+      std::cout << "Inserting" << std::endl;
+      move_group->move();
+      std::cout << "Inserted" << std::endl;
+
+      /*
+      for( int i=0; i<trajectory.joint_trajectory.points.size(); i++ ){
+	for( int j=0; j<trajectory.joint_trajectory.points[i].positions.size(); j++){
+	  //std::cout << std::setw(13) << trajectory.joint_trajectory.points[i].positions[j];
+	}
+	trajectory.joint_trajectory.points[i].time_from_start.sec *= 4;
+      }
+
+      auto action_client =
+	rclcpp_action::create_client<moveit_msgs::msg::ExecuteTrajectoryAction>
+	(this, "/execute_trajectory");
+      action_client->wait_for_action_server(std::chrono::seconds(2));
+
+      moveit_msgs::msg::ExecuteTrajectoryGoal goal;
+      goal.trajectory.joint_trajectory = trajectory.joint_trajectory;
+      auto goal_handle_future = action_client->async_send_goal(goal_msg);
+      if (rclcpp::spin_until_future_complete(this, goal_handle_future) !=
+	  rclcpp::FutureReturnCode::SUCCESS){
+      }
+      */
+    }
+  }
   
 };
 
 int main( int argc, char** argv ){
 
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<spine_control>());
+
+  auto move_group_node = std::make_shared<spine_control>();
+
+  rclcpp::executors::SingleThreadedExecutor executor;
+  executor.add_node(move_group_node);
+  std::thread([&executor]() { executor.spin(); }).detach();
+
+  move_group_node->create_mg();
   getchar();
   rclcpp::shutdown();
-
-  //auto node = std::make_shared<spine_control>("spinecontrol_node");
-
-  //ros::NodeHandle nh;
-  //ros::AsyncSpinner spinner(1);
-  //spinner.start();
-  
-  //spine_control control(nh);
-
-  //ros::spinOnce();
-  
   
   return 0;
 }
