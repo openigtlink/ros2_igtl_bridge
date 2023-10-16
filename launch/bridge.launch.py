44e74f29cc37166378fee5098dad37865579239c
maribernardes <maribernardes@gmail.com>
2023-10-16 15:49:42 -0400

    Included python launch

---
+++ launch/bridge.launch.py
@@ -0,0 +1,19 @@
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    ld = LaunchDescription()
+
+    igtl_bridge = Node(
+        package="ros2_igtl_bridge",
+        executable="igtl_node",
+        parameters=[
+            {"RIB_server_ip":"localhost"},
+            {"RIB_port": 18944},
+            {"RIB_type": "client"}
+        ]
+    )
+
+    ld.add_action(igtl_bridge)
+    return ld
